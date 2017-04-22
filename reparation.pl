84bc06a58d0039af519811dd2e7f8bc14cf5106a
ndah-e <elvis.ndah@gmail.com>
2017-04-22 13:44:55 +0200

    Plastid P-site estimation

--- reparation.pl
+++ reparation.pl
@@ -28,6 +28,8 @@ use diagnostics;
 use Getopt::Long;
 use Cwd;
 use File::stat;
+use File::Basename;
+
 
 
 ########################
@@ -41,9 +43,11 @@ use File::stat;
 
 # Mandatory variables
 my $genome;								# Prokaryotic genome file in fasta format
-my $Ribo_alignment;						# Ribosome profiling alignment file [sam formate]
+my $sam_file;						# Ribosome profiling alignment file [sam formate]
 my $blastdb;							# protein blast database in fasta format
-my $script_dir = "scripts";				# Directory where the script files are stored (defaults to the current directory)
+
+my $dirname = dirname(__FILE__);		# get tool directory for default script directory
+my $script_dir = $dirname."/scripts";	# Directory where the script files are stored (defaults to the current directory)
 
 # Input variables
 my $threads = 4;						# # number of threads used by the USEARCH tool
@@ -69,6 +73,14 @@ my $start_codon_nset = "CTG";			# Start codon for the negative set
 my $start_codon_pset = "ATG,GTG,TTG";	# Start codon for the positive set. Defualts to start codons set
 
 
+# Output files
+my $bedgraphS;
+my $bedgraphAS;
+my $predicted_ORFs;
+my $predicted_ORFs_bed;
+my $predicted_ORFs_fasta;
+
+
 # track processing time
 my $startRun = time();
 
@@ -76,7 +88,7 @@ my $startRun = time();
 GetOptions(
 	'g=s'=>\$genome,
 	'gtf=s'=>\$gtf,
-	'a=s'=>\$Ribo_alignment,
+	'sam=s'=>\$sam_file,
 	'sdir=s'=>\$script_dir,
 	'wdir=s'=>\$workdir,
 	'en=s'=>\$experiment,
@@ -92,26 +104,19 @@ GetOptions(
 	'seed=s'=>\$SEED,
 	'sd=i'=>\$USESD,
 	'id=f'=>\$identity,
-	'ev=i'=>\$evalue,
+	'ev=f'=>\$evalue,
 	'pg=i'=>\$pgm,
-	'pset=s'=>\$positive_set,
+	#'pset=s'=>\$positive_set,
 	'cdn=s'=>\$start_codons,
 	'ncdn=s'=>\$start_codon_nset,
-	'pcdn=s'=>\$start_codon_pset
+	'pcdn=s'=>\$start_codon_pset,
+	'bgS=s'=>\$bedgraphS,
+	'bgAS=s'=>\$bedgraphAS,
+	'orf=s'=>\$predicted_ORFs,
+	'bed=s'=>\$predicted_ORFs_bed,
+	'fa=s'=>\$predicted_ORFs_fasta
 );
 
-
-# Check for mandatory variables;
-my %params = (
-	g => $genome,
-	b => $Ribo_alignment,
-	sdir => $script_dir,
-	db => $blastdb
-);
-
-#check if mandatory parameters are properly initialized
-my @invalid = grep uninitialized_param($params{$_}), keys %params;	
-die "Not properly initialized: @invalid\n" if @invalid;
 
 
 # ensure start codons are uppercase
@@ -121,36 +126,45 @@ unless($start_codon_pset) {$start_codon_pset = $start_codons}
 
 $experiment = ($experiment) ? $experiment."_": "";
 
+# read sam/bam file
+# generate bed graph file and use it as occupancy 
+# sort bam file samtools sort aln.bam aln.sorted
+# genomeCoverageBed -bg -$occupancy -ibam $bam_file -strand - 
+
 # check if script directoryis properly initialized and contains all scripts
 if ($script_dir) {
-	$script_dir = (substr($script_dir, -1) eq '/') ? $script_dir: $script_dir."/"; # check and add forward slash
+	#$script_dir = (substr($script_dir, -1) eq '/') ? $script_dir: $script_dir."/"; # check and add forward slash
+	unless (-e $script_dir."/positive_set.pl") {
+		print "Script 'positive_set.pl' not found in script directory.\nEnsure the directory  '$script_dir' contains all required file (see readme).\n";
+		exit(1);
+	}
 
-	unless (-e $script_dir."Ribo_seq_occupancy.py") {
+	unless (-e $script_dir."/Ribo_seq_occupancy.py") {
 		print "Script 'Ribo_seq_occupancy.py' not found in script directory.\nEnsure the directory  '$script_dir' contains all required file (see readme).\n";
 		exit(1);
 	}
 
-	unless (-e $script_dir."generate_all_ORFs.pl") {
+	unless (-e $script_dir."/generate_all_ORFs.pl") {
 		print "Script 'generate_all_ORFs.pl' not found in script directory.\nEnsure the directory '$script_dir' contains all required file (see readme).\n";
 		exit(1);
 	}
 
-	unless (-e $script_dir."profiles.pl") {
+	unless (-e $script_dir."/profiles.pl") {
 		print "Script 'profiles.pl' not found in script directory.\nEnsure the directory '$script_dir' contains all required file (see readme).\n";
 		exit(1);
 	}
 
-	unless (-e $script_dir."plot_profile.R") {
+	unless (-e $script_dir."/plot_profile.R") {
 		print "Script 'plot_profile.R' not found in script directory.\nEnsure the directory '$script_dir' contains all required file (see readme).\n";
 		exit(1);
 	}
 
-	unless (-e $script_dir."Random_Forest.R") {
+	unless (-e $script_dir."/Random_Forest.R") {
 		print "Script 'Random_Forest.R' not found in script directory.\nEnsure the directory '$script_dir' contains all required file (see readme).\n";
 		exit(1);
 	}
 
-	unless (-e $script_dir."post_processing.pl") {
+	unless (-e $script_dir."/post_processing.pl") {
 		print "Script 'post_processing.pl' not found in script directory.\nEnsure the directory '$script_dir' contains all required file (see readme).\n";
 		exit(1);
 	}
@@ -169,11 +183,19 @@ if ($script_dir) {
 # Check if working directory exist
 my ($work_dir, $tmp_dir) = check_working_dir($workdir);
 
+
+
+# append work_dir to output files
+unless($bedgraphS) {$bedgraphS = $work_dir.$experiment."Ribo-seq_Sense_".$occupancy.".bedgraph";}
+unless($bedgraphAS) {$bedgraphAS = $work_dir.$experiment."Ribo-seq_AntiSense_".$occupancy.".bedgraph";}
+unless($predicted_ORFs) {$predicted_ORFs= $work_dir.$experiment."Predicted_ORFs.txt";}
+unless($predicted_ORFs_bed) {$predicted_ORFs_bed = $work_dir.$experiment."Predicted_ORFs.bed";}
+unless($predicted_ORFs_fasta) {$predicted_ORFs_fasta = $work_dir.$experiment."Predicted_ORFs.fasta";}
+
 # Check prerequisits
-if ($pgm == 1) {
+if ($pgm == 1 or $pgm == 2) {
 	$positive_set = $work_dir."positive_set.txt";
-} elsif ($pgm == 2) {
-	$positive_set = $work_dir."positive_set.txt";
+
 } elsif ($pgm == 3) {
 	if ($positive_set) {
 		if (-e $positive_set) {
@@ -188,18 +210,27 @@ if ($pgm == 1) {
 	}
 }
 
-# print input variables
-if ($identity <= 0 or $identity > 1) {
-	print "-id can only take a value greater than zero and less than or equal to 1\n";
-	exit;
+# generate positive set
+print "Generate positive set...\n";
+my $positive_set_gtf = $work_dir.'tmp/positive.gtf';
+my $cmd_positive = "perl ".$script_dir."/positive_set.pl $genome $blastdb $positive_set $min_read_len $max_read_len $MINORF $identity $evalue $start_codon_pset $pgm $work_dir $script_dir $threads";
+print "$cmd_positive\n";
+system($cmd_positive);
+print "Done.\n\n";
+
+# section to implement plastid
+my $psite_offset_file = "";
+if ($occupancy == 1) {
+    $psite_offset_file = generate_p_site($positive_set_gtf,$sam_file,$min_read_len,$max_read_len);
 }
+
 
 # Generate occupancy file
 print "Generating ribosome occupancy file..\n";
-my $bedgraphS = $work_dir.$experiment."Ribo-seq_Sense_".$occupancy."prime";
-my $bedgraphAS = $work_dir.$experiment."Ribo-seq_AntiSense_".$occupancy."prime";
 my $occupancyFile = $work_dir.$experiment."Ribo-seq_".$occupancy."_occupancy.txt";
-my $cmd_occupancy = "python ".$script_dir."Ribo_seq_occupancy.py $Ribo_alignment $occupancy $min_read_len $max_read_len $bedgraphS $bedgraphAS $occupancyFile";
+my $bedgraphS_prefix = $experiment."Ribo-seq_Sense_".$occupancy;
+my $bedgraphAS_prefix = $experiment."Ribo-seq_AntiSense_".$occupancy;
+my $cmd_occupancy = "python ".$script_dir."/Ribo_seq_occupancy.py $sam_file $occupancy $min_read_len $max_read_len $bedgraphS $bedgraphAS $occupancyFile $bedgraphS_prefix $bedgraphAS_prefix $psite_offset_file";
 print "$cmd_occupancy\n";
 system($cmd_occupancy);
 print "Done.\n\n";
@@ -209,31 +240,31 @@ print "Done.\n\n";
 print "Generate all possible ORFs...\n";
 my $codons = $start_codons.",".$start_codon_nset;	# combine the start codons
 my $ORF_file = $work_dir."tmp/all_valid_ORFs.txt";
-my $cmd_orf_gen = "perl ".$script_dir."generate_all_ORFs.pl $genome $blastdb $occupancyFile $positive_set $ORF_file $MINORF $OFFSET_START $OFFSET_STOP $OFFSET_SD $SEED $identity $evalue $codons $start_codon_pset $pgm $work_dir $script_dir $threads";
+my $cmd_orf_gen = "perl ".$script_dir."/generate_all_ORFs.pl $genome $blastdb $occupancyFile $positive_set $ORF_file $MINORF $OFFSET_START $OFFSET_STOP $OFFSET_SD $SEED $identity $evalue $codons $start_codon_pset $pgm $work_dir $script_dir $threads";
 print "$cmd_orf_gen\n";
 system($cmd_orf_gen);
 print "Done.\n\n";
 
 # Generate metagenic profile
 print "Meta-genic plots\n";
-my $cmd_meta = "perl ".$script_dir."profiles.pl $positive_set $occupancyFile $MINREAD $work_dir $script_dir";
+my $cmd_meta = "perl ".$script_dir."/profiles.pl $positive_set $occupancyFile $MINREAD $work_dir $script_dir";
 print "$cmd_meta\n";
 system($cmd_meta);
 print "Done.\n\n";
 
 # ORF prediction
 print "Performing ORF prediction analysis..\n";
-my $RF_command = "Rscript ".$script_dir."Random_Forest.R $ORF_file $positive_set $work_dir $start_codons $start_codon_nset $USESD";
+my $RF_prediction = $work_dir."tmp/RF_predicted_ORFs.txt";
+my $threshold = $work_dir."tmp/threshold.txt";
+my $RF_command = "Rscript ".$script_dir."/Random_Forest.R $ORF_file $positive_set $work_dir $start_codons $start_codon_nset $USESD";
 print "$RF_command\n";
 system($RF_command);
 print "Done.\n\n";
 
 # Post processing
 print "Cleaning up RF predictions..\n";
-my $threshold = $work_dir."tmp/threshold.txt";
-my $RF_prediction = $work_dir."tmp/RF_predicted_ORFs.txt";
 my $output_prefix = $work_dir.$experiment."Predicted_ORFs";
-my $processing_cmd = "perl ".$script_dir."post_processing.pl $RF_prediction $genome $occupancyFile $output_prefix $threshold $OFFSET_START $MINREAD $gtf";
+my $processing_cmd = "perl ".$script_dir."/post_processing.pl $RF_prediction $genome $occupancyFile $output_prefix $threshold $OFFSET_START $MINREAD $gtf $predicted_ORFs $predicted_ORFs_bed $predicted_ORFs_fasta";
 print "$processing_cmd\n";
 system($processing_cmd);
 print "Done.\n\n";
@@ -247,6 +278,43 @@ timer($startRun);	# Get total Run time
 ##	SUBS
 ##################
 
+## Generate metagene and p-site estimates
+sub generate_p_site {
+
+    my $genes_gtf = $_[0];
+    my $sam = $_[1];
+    my $min_l = $_[2];
+    my $max_l =$_[3];
+
+
+    my $run_name = $work_dir."tmp/plastid";
+
+    # convert sam to bam and index
+    my $bam = $work_dir."tmp/ribo_bam.bam";
+    my $cmd_sam2bam = "samtools view -bS $sam | samtools sort -o $bam";
+    system($cmd_sam2bam);
+
+    my $command_index = "samtools index ".$bam;
+    system($command_index);
+    
+    #Build command
+    my $command_meta = "metagene generate -q ".$run_name." --landmark cds_start --annotation_files ".$genes_gtf." 2> /dev/null";
+    print "Generate metagene\n".$command_meta."\n\n";
+    system($command_meta);
+
+    #Build command
+    my $psitefile = $run_name."_rois.txt";
+    my $command_psite = "psite -q ".$run_name."_rois.txt ".$run_name." --min_length ".$min_l." --max_length ".$max_l." --require_upstream --count_files ".$bam." 2> /dev/null";
+    print "Calculate psite\n".$command_psite."\n\n";
+    system($command_psite);
+
+    system("mv ".$run_name."_p_offsets.txt ".$work_dir.$experiment."p_offsets.txt");
+    system("mv ".$run_name."_p_offsets.png ".$work_dir.$experiment."p_offsets.png");
+
+    return $work_dir.$experiment."p_offsets.txt";
+}
+
+
 sub check_if_pgm_exist {
 
 	my $pgm = $_[0];
@@ -255,13 +323,13 @@ sub check_if_pgm_exist {
 	chomp($search);
 	if ($search =~ /^which: no/) {
 		if ($pgm eq "prodigal") {
-			unless (-e $script_dir."bin/prodigal") {	# if prodigal not install
-				print "Could not locate ' $pgm '. Please ensure it is installed and executable\n";
+			unless (-e $script_dir."/bin/prodigal") {	# if prodigal not install
+				print "Could not locate ' $pgm '. Please ensure it is installed and in the path or present in the script directory\n";
 				exit(1);
 			}
 		} elsif ($pgm eq "glimmer3") {
-			unless (-e $script_dir."bin/glimmer/glimmer3" and -e $script_dir."bin/glimmer/bin/build-icm") {
-				print "Could not locate '$pgm'. Please ensure it is installed and executable\n";
+			unless (-e $script_dir."/bin/glimmer/glimmer3" and -e $script_dir."bin/glimmer/bin/build-icm") {
+				print "Could not locate ' $pgm '. Please ensure it is installed and in the path or present in the script directory\n";
 				exit(1);
 			}
 		}
@@ -320,4 +388,5 @@ sub timer {
 	printf("\nTotal running time: %02d:%02d:%02d\n\n", int($runTime / 3600), int(($runTime  % 3600) / 60), int($runTime % 60));
 
 }
+
 
